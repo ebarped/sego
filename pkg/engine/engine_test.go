eaff5fa46dcd6d83dccf5e9a5329b5174f94fc92
ebarped <eduardo.barajas.pedrosa@gmail.com>
2023-04-21 11:33:53 +0200

    engine: add Search benchmark

--- pkg/engine/engine_test.go
+++ pkg/engine/engine_test.go
@@ -1,6 +1,20 @@
 package engine
 
-import "testing"
+import (
+	"testing"
+)
+
+const DOCS_DIR = "../../linux-kernel-docs"
+
+var e *Engine
+
+func init() {
+	e = New()
+	e.Load(DOCS_DIR)
+}
 
 func BenchmarkSearch(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		e.Search("memory management")
+	}
 }
